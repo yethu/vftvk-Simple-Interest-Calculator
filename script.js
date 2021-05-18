0cc5de91307b75bcb768b84d7af4f0090c1c9d71
Ye Thu <yethu.personal@gmail.com>
2021-05-18 21:44:23 +0630

    Change implementation according to the marking guideline

--- script.js
+++ script.js
@@ -17,16 +17,9 @@ function compute() {
   var interest = (principal * years * rate) / 100;
   var year = new Date().getFullYear() + parseInt(years);
 
-  document.getElementById('result').innerHTML =
-    'If you deposit ' +
-    principal +
-    ',<br>at an interest rate of ' +
-    rate +
-    '%<br>You will receive an amount of ' +
-    interest +
-    ',<br>in the year ' +
-    year +
-    '<br>';
+  document.getElementById(
+    'result'
+  ).innerHTML = `If you deposit <span class="highlight">${principal}</span>,<br>at an interest rate of <span class="highlight">${rate}%</span><br>You will receive an amount of <span class="highlight">${interest}</span>,<br>in the year <span class="highlight">${year}</span><br>`;
 }
 
 /**
@@ -34,5 +27,5 @@ function compute() {
  */
 function updateRate() {
   var rateval = document.getElementById('rate').value;
-  document.getElementById('rate_val').innerText = rateval;
+  document.getElementById('rate_val').innerText = `${rateval}%`;
 }
